d949f0e61005a172d1b3db49dfde35f57e8b730b
RaynaldoHuang <huangraynaldo@gmail.com>
2025-02-10 02:12:58 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,219 @@
+"use client"
+
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import logo from "@/public/png/logo.png"
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+import { useState } from "react";
+import { productData } from "@/libs/product";
+import React from "react";
+import Link from "next/link";
+
+import wa from "@/public/whatsapp.svg"
+import spay from "@/public/shopeepay.svg"
+import bca from "@/public/bca.svg"
+import permata from "@/public/permata.svg"
+import gopay from "@/public/gopay.svg"
+import dana from "@/public/dana.svg"
+import qris from "@/public/qris.svg"
+import jne from "@/public/jne.svg"
+import idex from "@/public/idex.svg"
+import jt from "@/public/jt.svg"
+import sicepat from "@/public/sicepat.svg"
+import lion from "@/public/lion.svg"
+import sap from "@/public/sap.svg"
+import ninja from "@/public/ninja.svg"
+
+export default function Home() {
+  const products = productData
+
+  const [selectedImage, setSelectedImage] = useState(products.images[0]);
+
+  return (
+    <>
+      <div className="max-w-[425px] mx-auto">
+        <div className="flex justify-between items-center py-6 mx-4">
+          <Image
+            alt=""
+            src={logo}
+            className="w-28"
+          />
+
+          <div className="flex">
+            <Link
+              href={""}
+              className="bg-green-700 text-white py-2 px-4 flex items-center rounded-lg"
+            >
+              <Image
+                alt=""
+                src={wa}
+                className="w-6 me-2"
+              />
+              Ambil Diskon
+            </Link>
+          </div>
+        </div>
+
+        <div className="mx-4">
+          {/* Bagian Gambar */}
+          <div>
+            <Image
+              src={selectedImage}
+              alt="Product"
+              className="w-full h-96 object-cover shadow"
+            />
+
+            <div className="flex mt-4 space-x-3">
+              {
+                products.images.map((image, index) => (
+                  <div key={index}>
+                    <div className="flex-col justify-center rounded-[10px]">
+                      <div className='w-full'>
+                        <Image
+                          key={index}
+                          src={image}
+                          alt=''
+                          className={`w-18 h-18 object-cover rounded cursor-pointer border-2 ${selectedImage === image ? "border-blue-500" : "border-gray-300"
+                            }`}
+                          onClick={() => setSelectedImage(image)}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                ))
+              }
+            </div>
+
+            <div className="flex space-x-2">
+              <div className="mt-4 py-2 px-2 bg-black text-white text-sm">
+                Ada Stock
+              </div>
+              <div className="mt-4 py-2 px-2 bg-red-900 text-white text-sm">
+                Flash Sale 50%
+              </div>
+            </div>
+
+            <h1 className='mt-4 text-2xl text-[#1F2D3E] font-semibold'>{products.name}</h1>
+
+            <div className="flex items-center mt-2">
+              <h1 className='text-3xl text-[#1F2D3E] font-semibold me-3'>{products.price}</h1>
+              <h1 className='text-md text-[#1F2D3E] font-semibold line-through decoration-red-800'>Rp.359.000</h1>
+            </div>
+          </div>
+        </div>
+        <div className="border-t border-gray-300 my-6"></div>
+
+        <div className="mx-4 mb-8">
+          <p>{products.description.split("\n").map((line, index) => (
+            <React.Fragment key={index}>
+              {line}
+              <br />
+            </React.Fragment>
+          ))}</p>
+        </div>
+
+        <div className="mx-4 mb-10">
+          <h1 className="font-semibold text-based mb-4">Metode Pembayaran :</h1>
+
+          <div>
+            <div className="flex space-x-4 items-center mb-5">
+              <p className="font-extrabold text-red-900 text-xl">COD</p>
+              <Image
+                src={qris}
+                alt="Product"
+                className="w-32"
+              />
+              <Image
+                src={bca}
+                alt="Product"
+                className="w-16"
+              />
+              <Image
+                src={spay}
+                alt="Product"
+                className="w-16"
+              />
+            </div>
+            <div className="flex space-x-4 items-center">
+              <Image
+                src={permata}
+                alt="Product"
+                className="w-28"
+              />
+              <Image
+                src={gopay}
+                alt="Product"
+                className="w-20"
+              />
+              <Image
+                src={dana}
+                alt="Product"
+                className="w-20"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="mx-4 mb-28">
+          <h1 className="font-semibold text-based mb-4">Ekspedisi Pengiriman :</h1>
+
+          <div>
+            <div className="flex space-x-4 items-center mb-5">
+              <Image
+                src={jne}
+                alt="Product"
+                className="w-20"
+              />
+              <Image
+                src={idex}
+                alt="Product"
+                className="w-14"
+              />
+              <Image
+                src={jt}
+                alt="Product"
+                className="w-24"
+              />
+              <Image
+                src={sicepat}
+                alt="Product"
+                className="w-14"
+              />
+            </div>
+            <div className="flex space-x-4 items-center">
+              <Image
+                src={ninja}
+                alt="Product"
+                className="w-16"
+              />
+              <Image
+                src={sap}
+                alt="Product"
+                className="w-16"
+              />
+              <Image
+                src={lion}
+                alt="Product"
+                className="w-20"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="relative">
+          <Link
+            href={""}
+            className="fixed bottom-6 right-4 left-4 bg-black text-white px-6 py-3 rounded-full shadow-lg transition-all text-lg font-semibold flex items-center justify-center"
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              alt=""
+              src={wa}
+              className="w-7 me-2"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Checkout Sekarang
+          </Link>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
   );
 }
---
+++ libs/product.ts
@@ -0,0 +1,21 @@
+import img1 from "@/public/png/img1.jpg"
+import img2 from "@/public/png/img2.jpg"
+import img3 from "@/public/png/img3.jpg"
+import img4 from "@/public/png/img4.jpg"
+import img5 from "@/public/png/img5.jpg"
+
+const productData = {
+    id: 1,
+    name: "Venda Bag Leather Perempuan",
+    description: "Tas Venda dibuat dari PU leather premium yang menawarkan tampilan mewah dengan daya tahan tinggi. Material ini lembut, tahan air, dan mudah dibersihkan, sehingga tas tetap terlihat elegan dalam berbagai kondisi.\n\nDilengkapi dengan jahitan rapi dan kuat, serta resleting berkualitas tinggi, tas ini tidak hanya stylish tetapi juga fungsional. Ruang penyimpanannya luas dan dirancang untuk memenuhi kebutuhan harian Anda, baik untuk bekerja, bepergian, atau acara formal.\n\nDeskripsi Barang :\nUkuran : 20 x 16\nBahan : PU Leather\nBerat : 0.4 kg",
+    price: "Rp.159.000",
+    images: [
+        img1,
+        img4,
+        img5,
+        img2,
+        img3,
+    ],
+};
+
+export { productData }
